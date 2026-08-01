8b960fe17b1a5159cdb9ddad7f0684ef0e1bde3e
Muhammed Berk Önder <70864966+berkmonder@users.noreply.github.com>
2026-08-01 15:17:16 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://berkmonder.github.io',
+  site: 'https://berkmonder.com',
 })
