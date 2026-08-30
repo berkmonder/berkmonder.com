53b043ecfc7205077cbf645fb7f095b1e23ca95a
BerkÖnder <berkonder@BerkOnders-MacBook-Pro.local>
2026-08-30 15:15:13 +0300

    update

--- astro.config.ts
+++ astro.config.ts
@@ -1,23 +1,23 @@
 import mdx from "@astrojs/mdx";
-import node from "@astrojs/node";
+// import node from "@astrojs/node";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import expressiveCode from "astro-expressive-code";
-import { loadEnv } from "vite";
-import spectre, { type GiscusMapping } from "./package/src";
+// import { loadEnv } from "vite";
+// import spectre, { type GiscusMapping } from "./package/src";
 // import { spectreDark } from "./src/ec-theme";
 
-const {
-	GISCUS_REPO,
-	GISCUS_REPO_ID,
-	GISCUS_CATEGORY,
-	GISCUS_CATEGORY_ID,
-	GISCUS_MAPPING,
-	GISCUS_STRICT,
-	GISCUS_REACTIONS_ENABLED,
-	GISCUS_EMIT_METADATA,
-	GISCUS_LANG,
-} = loadEnv(process.env.NODE_ENV!, process.cwd(), "");
+// const {
+// 	GISCUS_REPO,
+// 	GISCUS_REPO_ID,
+// 	GISCUS_CATEGORY,
+// 	GISCUS_CATEGORY_ID,
+// 	GISCUS_MAPPING,
+// 	GISCUS_STRICT,
+// 	GISCUS_REACTIONS_ENABLED,
+// 	GISCUS_EMIT_METADATA,
+// 	GISCUS_LANG,
+// } = loadEnv(process.env.NODE_ENV!, process.cwd(), "");
 
 // https://astro.build/config
 const config = defineConfig({
@@ -58,9 +58,9 @@ const config = defineConfig({
 			// },
 		}),
 	],
-	adapter: node({
-		mode: "standalone",
-	}),
+	// adapter: node({
+	// 	mode: "standalone",
+	// }),
 });
 
 export default config;
